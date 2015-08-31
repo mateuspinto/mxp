08e2586ae249706ea1e5300afbe5061bbcf6ed49
Joel Vandergriendt <joel@vectorblox.com>
2015-08-31 15:12:44 -0700

    Incorporate Guys changes to simple

--- examples/software/simple/test-cpp.cpp
+++ examples/software/simple/test-cpp.cpp
@@ -4,26 +4,23 @@
 
 const int num_elements=10;
 
-int main() {
+int main()
+{
 
 #if VBX_SIMULATOR==1
 	//initialize with 4 lanes,and 64kb of sp memory
 	//word,half,byte fraction bits 16,15,4 respectively
-	vbxsim_init(4,64,256,16,15,4);
+	vbxsim_init( 4, 64, 256, 16, 15, 4 );
 #endif
+
 	//Allocating vectors in scratchpad
 	VBX::Vector<vbx_word_t> a(num_elements);
 	VBX::Vector<vbx_word_t> b(num_elements);
 	VBX::Vector<vbx_word_t> c(num_elements);
 
-	//a = [1,2,3,...,10]
-	a=1+VBX::ENUM;
-
-	//b = [4,4,....,4]
-	b=4;
-
-	//c = a * b
-	c=a*b;
+	a = 1 + VBX::ENUM; //a = [1,2,3,...,10]
+	b = 4;             //b = [4,4,....,4]
+	c = a * b;
 
 	//wait for all vector instructions to finish
 	vbx_sync();
